be544d39eec55fa362625c5f38a233200458b5b5
Pedro Artur (Motsu) <71746213+Pedro-Artur-RF@users.noreply.github.com>
2023-05-31 12:43:55 -0300

    ...

---
+++ Recuperação/script.js
@@ -0,0 +1,7 @@
+    var post = document.getElementsByClassName("post")
+    var modalCtt = document.getElementById("modal-contact");
+    var btn = document.getElementById("btn");
+    var cancel = document.getElementsByClassName("close");
+        btn.onclick = function() { modalCtt.style.display = "block"; }
+        cancel.onclick = function() { modal.style.display = "none"; }
+        
